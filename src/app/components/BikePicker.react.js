5d249156c4c8face7cf552519cd41698b39d3b5a
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-01-31 10:54:59 -0800

    Scaffolding dropdown menu

--- src/app/components/BikePicker.react.js
+++ src/app/components/BikePicker.react.js
@@ -3,18 +3,30 @@ import React from 'react';
 export default class App extends React.Component {
 
   render() {
+    const size = ['small', 'medium', 'large', 'x-large']
+    const color = ['black', 'slime']
+    const buildKit = ['FRAME ONLY', 'ST', 'XT-01']
 
     return (
       <div>
-        <div>
-          Toggle1
-        </div>
-        <div>
-          Toggle2
-        </div>
-        <div>
-          Toggle3
-        </div>
+        <nav className="menu size">
+          <span className="menu-heading">SIZE</span>
+          <a className="menu-item">OP1</a>
+          <a className="menu-item">OP2</a>
+          <a className="menu-item">OP3</a>
+        </nav>
+        <nav className="menu color">
+          <span className="menu-heading">COLOR</span>
+          <a className="menu-item">OP1</a>
+          <a className="menu-item">OP2</a>
+          <a className="menu-item">OP3</a>  
+        </nav>  
+        <nav className="menu build-kit">
+          <span className="menu-heading">BUILD</span>
+          <a className="menu-item">OP1</a>
+          <a className="menu-item">OP2</a>
+          <a className="menu-item">OP3</a>  
+        </nav>
       </div>  
     );
   }
