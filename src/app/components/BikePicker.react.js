c865bf4063df05c9e59afb510b79d42dd1157f34
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-01-31 10:10:15 -0800

    Import component

---
+++ src/app/components/BikePicker.react.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <div>
+          Toggle1
+        </div>
+        <div>
+          Toggle2
+        </div>
+        <div>
+          Toggle3
+        </div>
+      </div>  
+    );
+  }
+}
--- src/app/pages/home/container/Home.js
+++ src/app/pages/home/container/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Immutable from 'seamless-immutable';
 import pureRender from '../../../decorators/pureRender';
+import BikePicker from '../../../components/BikePicker.react.js';
 
 @pureRender
 export default class Home extends React.Component {
@@ -10,6 +11,7 @@ export default class Home extends React.Component {
       <div id="home">
         <h1>EVIL</h1>
         <h3>Component Test</h3>
+        <BikePicker />
       </div>
     );
   }
