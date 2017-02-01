253d5a84d1f982e560ee8bfc25edaf69a175e943
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-01-31 16:50:23 -0800

    Using state to change render

--- src/app/components/BikePicker.react.js
+++ src/app/components/BikePicker.react.js
@@ -2,47 +2,59 @@ import React from 'react';
 
 export default class App extends React.Component {
 
-  render() {
-    const size = ['small', 'medium', 'large', 'x-large']
-    const color = ['black', 'slime']
-    const buildKit = ['FRAME ONLY', 'ST', 'XT-01']
 
+  constructor(){
+    super();
+    this.state = { size: [] }
+  }
+
+  componentWillMount() {
+    let size = ['small', 'medium', 'large', 'x-large']
+      // size: ['small', 'medium', 'large', 'x-large'],
+      // color: ['black', 'slime'],
+      // buildKit: ['FRAME ONLY', 'ST', 'XT-01']
+    
+    this.setState({size});
+  } 
+  render() {
+    let size = this.state.size
+    console.log(size);
     return (
       <form>
         <dl className="form-group">
           <dt><label>SIZE</label></dt>
           <dd>
           <select className="form-select">
-            <option>SMALL</option>
-            <option>OP2</option>
-            <option>OP3</option>
-          </select>
-          </dd>
-        </dl>
-        <dl className="form-group">
-          <dt><label>COLOR</label></dt>
-          <dd>
-          <select className="form-select">
-            <option>BLACK</option>
-            <option>OP2</option>
-            <option>OP3</option>
+            {size.map(s => <Size size={s}/> )} 
           </select>
           </dd>
         </dl>
 
-      
+        {/*<dl className="form-group">
+          <dt><label>COLOR</label></dt>
+          <dd>
+          <select className="form-select">
+            {bikeOptions.color.map(size => 
+              <option key={bikeOptions.color}>{bikeOptions.color}</option>
+            )}
+          </select>
+          </dd>
+        </dl>
+
         <dl className="form-group">
           <dt><label>BUILD KIT</label></dt>
           <dd>
           <select className="form-select">
-            <option>FRAME ONLY</option>
-            <option>OP2</option>
-            <option>OP3</option>
+            {bikeOptions.buildKit.map(size => 
+              <option key={bikeOptions.buildKit}>{bikeOptions.buildKit}</option>
+            )}
           </select>
           </dd>
-        </dl>
+        </dl>*/}
 
       </form>  
     );
   }
 }
+
+const Size = (props) => <option>{props.size}</option>
